e7b745017c832a3a637fa81f4e599889cc9c8477
Rafata8 <rtimermans@conwet.com>
2019-05-13 11:05:45 +0200

    solo enlazado

--- myapp/views.py
+++ myapp/views.py
@@ -9,35 +9,36 @@ from django.contrib.auth import authenticate
 #     return HttpResponse(template.render())
 
 def index(request):
-    # template = loader.get_template("myapp/login.html")
-    # return HttpResponse(template.render())
+    template = loader.get_template("myapp/index.html")
+    return HttpResponse(template.render())
     # return render(request, "myapp/index.html")
-    return render(request, "myapp/index.html")
+    # prueba('hola')
+    # return render(request, "myapp/index.html")
 
-
-def pelis(request):
-    if request.method == 'POST':
-        username = request.POST['usuario']
-        password = request.POST['contrasena']
-        user = authenticate(request,username=username,password=password)
-        if user is not None:
-            # print ('holaaa')
-            # login(request,user)
-            template = loader.get_template("myapp/pelis.html")
-            return render(request, "myapp/pelis.html")
-        else:
-            # # Return an 'invalid login' message.
-            # print ('adioos')
-            template = loader.get_template("myapp/login.html")
-            return HttpResponse(template.render())
-        
-def prueba(request):
-    print ('yaaa veees')
-    pelis(request)
 
 # def pelis(request):
-#     template = loader.get_template("myapp/pelis.html")
-#     return HttpResponse(template.render())
+#     if request.method == 'POST':
+#         username = request.POST['usuario']
+#         password = request.POST['contrasena']
+#         user = authenticate(request,username=username,password=password)
+#         if user is not None:
+#             # print ('holaaa')
+#             # login(request,user)
+#             template = loader.get_template("myapp/pelis.html")
+#             return render(request, "myapp/pelis.html")
+#         else:
+#             # # Return an 'invalid login' message.
+#             # print ('adioos')
+#             template = loader.get_template("myapp/login.html")
+#             return HttpResponse(template.render())
+        
+# def prueba(hola):
+#     print ('yaaa veees')
+#     # pelis(request)
+
+def pelis(request):
+    template = loader.get_template("myapp/pelis.html")
+    return HttpResponse(template.render())
 
 def administrar_pelis(request):
     template = loader.get_template("myapp/administrar_pelis.html")
